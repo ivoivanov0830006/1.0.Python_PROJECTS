7b31eee98507a15d9a3f35be95ca1ecab2d1e49f
ivoivanov0830006 <108527062+ivoivanov0830006@users.noreply.github.com>
2022-09-10 23:06:31 +0300

    Update jump_shoot.py

--- jump_shoot/jump_shoot.py
+++ jump_shoot/jump_shoot.py
@@ -1,14 +1,16 @@
 import pygame
 pygame.init()
 
-win = pygame.display.set_mode((500, 500))
+screen_width = 500
+screen_height = 500
+win = pygame.display.set_mode((screen_width, screen_height))
 
 pygame.display.set_caption("Jump'n'Shoot")
 
 x = 50
-y = 50
-width = 40
-height = 60
+y = 400
+player_width = 40
+player_height = 60
 velocity = 15
 
 run = True
@@ -21,17 +23,17 @@ while run:
 
     keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_LEFT]:
+    if keys[pygame.K_LEFT] and x > 0:
         x -= velocity
-    if keys[pygame.K_RIGHT]:
+    if keys[pygame.K_RIGHT] and x < screen_width - player_width:
         x += velocity
-    if keys[pygame.K_UP]:
+    if keys[pygame.K_UP] and y > 0:
         y -= velocity
-    if keys[pygame.K_DOWN]:
+    if keys[pygame.K_DOWN] and y < screen_height - player_height:
         y += velocity
 
     win.fill((0, 0, 0))
-    pygame.draw.rect(win, (255, 0, 0), (x, y, width, height))
+    pygame.draw.rect(win, (255, 0, 0), (x, y, player_width, player_height))
     pygame.display.update()
 
 pygame.quit()
