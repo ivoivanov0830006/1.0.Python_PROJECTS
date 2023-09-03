628dd1d1302690b7e99211527a01b8effa4bf0b4
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-09-03 22:00:03 +0300

    Update auth.py

--- web_project/website/auth.py
+++ web_project/website/auth.py
@@ -1,18 +1,17 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request
 
 auth = Blueprint("auth", __name__)
 
-
-@auth.route('/login')
+@auth.route('/login', methods=["GET", "POST"])
 def login():
-    return "<p>login</p>"
+    data = request.form
+    print(data)
+    return render_template("index.html", bolean=True)
 
+@auth.route('/register', methods=["GET", "POST"])
+def register():
+    data = request.form
+    print(data)
+    # return render_template("register.html")
 
-@auth.route('/logout')
-def logout():
-    return "<p>logout</p>"
-
-
-@auth.route('/sign-up')
-def sign_up():
-    return "<p>sign up</p>"
+# Add more authentication routes as needed
