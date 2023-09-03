1d8061f5754a46c2eb2777132e30344857fafae0
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-09-03 22:00:29 +0300

    Update views.py

--- web_project/website/views.py
+++ web_project/website/views.py
@@ -2,6 +2,14 @@ from flask import Blueprint, render_template
 
 views = Blueprint("views", __name__)
 
-@views.route("/")
+@views.route('/')
 def home():
     return render_template("index.html")
+
+@views.route('/contacts')  # Define a route for the contacts page
+def contacts():
+    return render_template("contacts.html")
+
+@views.route('/shop')  # Define a route for the shop page
+def shop():
+    return render_template("shop.html")
