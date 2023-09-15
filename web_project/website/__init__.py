5d99baebb572ecd1b561d1c56db67e07c2ede83a
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-09-15 23:17:00 +0300

    Update __init__.py

--- web_project/website/__init__.py
+++ web_project/website/__init__.py
@@ -1,14 +1,19 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = "21e49d48"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
+
+    db.init_app(app)
 
     from .views import views
     from .auth import auth
 
-    app.register_blueprint(views, url_prefix="/")
-    app.register_blueprint(auth, url_prefix="/")
+    app.register_blueprint(views)
+    app.register_blueprint(auth)
 
     return app
