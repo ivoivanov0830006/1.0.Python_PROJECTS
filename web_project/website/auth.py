1bb37a3b47e82d75f792538cb99f37859e93004c
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-09-21 22:28:15 +0300

    Update auth.py

--- web_project/website/auth.py
+++ web_project/website/auth.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash, session
 import sqlite3
 from .models import User  # Import the User model
-from flask_bcrypt import Bcrypt, check_password_hash
+from flask_bcrypt import Bcrypt
 
 # Create the authentication blueprint
 auth = Blueprint("auth", __name__)
@@ -25,12 +25,32 @@ conn.commit()
 bcrypt = Bcrypt()
 
 
-def authenticate_user(email, password):
-    user = User.query.filter_by(email=email).first()
-    if user and check_password_hash(user.password, password):
-        return user
-    return None
+def authenticate_user(username, password):
+    # Print all users in the database (before the query)
+    all_users = User.query.all()
+    print("All users in the database:")
+    for user in all_users:
+        print(f"Username: {user.username}, Password: {user.password}")
 
+    print(f"Authenticating user: {username}")
+    user = User.query.filter_by(username=username).first()
+    if user:
+        print(f"User found: {user.username}")
+        if user.password == password:
+            print("Password matches")
+            return user
+        else:
+            print("Password does not match")
+    else:
+        print("User not found")
+
+    # Print all users in the database (after the query)
+    all_users = User.query.all()
+    print("All users in the database after query:")
+    for user in all_users:
+        print(f"Username: {user.username}, Password: {user.password}")
+
+    return None
 
 @auth.route('/indexLoginR')
 def index_login():
@@ -52,10 +72,10 @@ def logout():
 @auth.route('/login', methods=["POST"])
 def login():
     if request.method == "POST":
-        email = request.form.get("email")
+        username = request.form.get("username")
         password = request.form.get("password")
-
-        user = authenticate_user(email, password)
+        print(f"Username: {username}, Password: {password}")
+        user = authenticate_user(username, password)
 
         if user:
             # Store user ID in the session to keep the user logged in
@@ -76,16 +96,13 @@ def register():
         email = request.form.get("email")
         password = request.form.get("password")
 
-        # Hash the password using bcrypt
-        hashed_password = bcrypt.generate_password_hash(password).decode("utf-8")
-
         # Create a new database connection and cursor for this request
         conn = sqlite3.connect("users.db")
         cursor = conn.cursor()
 
         try:
             cursor.execute("INSERT INTO users (username, email, password) VALUES (?, ?, ?)",
-                           (username, email, hashed_password))
+                           (username, email, password))  # Store the plain text password
             conn.commit()
             flash("Registration successful. You can now log in.", "success")
             print("Register successful")
@@ -97,6 +114,5 @@ def register():
             # Close the database connection and cursor
             cursor.close()
             conn.close()
-
     return render_template("index.html")
 
